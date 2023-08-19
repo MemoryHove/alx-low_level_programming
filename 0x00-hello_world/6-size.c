776de116f84cae5c16922dbbe9aa4dde74279865
MemoryHove <memlety@yahoo.com>
2023-08-19 15:59:41 -0700

    task six

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point 
+ *
+ * return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("size of a char: %iu byte(s)\n", size of (char));
+	printf("size of an int: %iu byte(s)\n", size of (int));
+	printf("size of a long int: %iu byte(s)\n", size of (long int));
+	printf("size of a long long int: %iu byte(s)\n", size of (long long int));
+	printf("size of a float: %iu byte(s)\n", size of (float));
+	return (0);
+
