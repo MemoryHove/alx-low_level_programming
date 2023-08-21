7e3a79fc95044c419c2bb90a1fa135b5ffe69aff
MemoryHove <memlety@yahoo.com>
2023-08-21 13:41:31 -0700

    Task 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,6 +8,7 @@
  * Return - always (0)
  *
  */
+
 int main(void)
 {
 	int n;
@@ -17,15 +18,15 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("%d is positive\n", n) ;
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n", n) ;
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("%d is negative\n", n) ;
+		printf("%d is negative\n", n);
 	}
 	return (0);
 } 
